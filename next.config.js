cd02b9302a7d620c0f657cdfc67b6290406335b4
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2021-09-10 16:01:57 -0700

    Staging prefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const PROD = process.env.NODE_ENV === 'production';
+
 module.exports = {
+  assetPrefix: PROD ? 'https://chanzuckerberg.github.io/napari.github.io' : '',
+
   webpack(config) {
     // Sets BABEL_ENV to `client` or `server` depending on the Next.js build.
     // This is required for the Material UI + babel import plugin to work.
